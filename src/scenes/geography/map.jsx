e91834cf7972c721c441106cc4eed597ec58abae
KUMARI AMISHA <114377317+Amisha727@users.noreply.github.com>
2024-09-13 00:46:15 +0530

    first commit

---
+++ src/scenes/geography/map.jsx
@@ -0,0 +1,32 @@
+import { Box, useTheme } from "@mui/material";
+import delhiMap from "../../assets/delhi.jpg";
+import GeographyChart from "../../components/GeographyChart";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+
+const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="Geography" subtitle="Simple Geography Chart" />
+
+      {/* Box containing the image */}
+      <Box>
+        <img 
+          src={delhiMap}
+          alt="Delhi SLDC Network Map" 
+          style={{ maxWidth: '100%', height: 'auto' }} // Makes the image responsive
+        />
+      </Box>
+
+      {/* Geography Chart Component */}
+      <Box mt="20px">
+        <GeographyChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Geography;
